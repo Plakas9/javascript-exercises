c40539b7f8c33af73aebd1853ef894eb06d24ef4
plakas9 <plakas9@gmail.com>
2023-07-31 16:31:20 +0200

    ALL Solutions passed!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
 const removeFromArray = function() {
+    
+    let inputArr = arguments[0];
+    
+    for(let i=1; i<arguments.length; i++){
+        
+        for(let j = 0; j < inputArr.length; j++ ){
+            if((inputArr[j]==arguments[i])&& typeof inputArr[j] == typeof arguments[i]){
+                inputArr.splice(j,1);
+                
+            }
+        }
+    }
+    return inputArr;
 
 };
 
