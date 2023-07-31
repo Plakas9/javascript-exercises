ab132674525f370d5e160babe896c910f13bb793
plakas9 <plakas9@gmail.com>
2023-07-31 17:45:24 +0200

    completed conversion to both farenheit and celcius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+
+const convertToFahrenheit = function(x) {
+  return Math.round(10*(x = (x*9/5+32)))/10; 
+
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(y) {
+  return Math.round((y = 5/9*(y-32))*10)/10;
 };
 
 // Do not edit below this line
