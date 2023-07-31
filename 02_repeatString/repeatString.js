9264a8c2f0781d6db2692bbeaf87531ec1bf200e
plakas9 <plakas9@gmail.com>
2023-07-31 14:42:08 +0200

    First attempt at solution passed first 4 cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    const joinArray = [];
+    for(let i =  0; i< times; i++){
+        joinArray.push(word);
+    }
+    return joinArray.join("")   ;
 };
 
 // Do not edit below this line
