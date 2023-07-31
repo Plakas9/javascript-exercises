a3ad53e6a130d388a9826c16f83bc4871fd9fe15
plakas9 <plakas9@gmail.com>
2023-07-31 16:49:15 +0200

    fixed case so error will display if negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -6,13 +6,15 @@ const sumAll = function(number1,number2) {
         n2 = number1; // n1 will always be the smaller number
         n1=number2;
     }
-    console.log(n1,n2);
-
-    for(let i = n1; i<n2+1; i++){
-        total = total + i;
+    if((n1 || n2 )<0){
+        return "ERROR"
+    }else{
+        
+        for(let i = n1; i<n2+1; i++){
+            total = total + i;
+        }
+        return total;
     }
-    return total;
-
 };
 
 // Do not edit below this line
