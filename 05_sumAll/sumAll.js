8014876f34bfa78ff03d9ea2110623e6740e4773
plakas9 <plakas9@gmail.com>
2023-07-31 17:10:04 +0200

    passed case where types do not match

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,13 +2,21 @@ const sumAll = function(number1,number2) {
     let n1 = number1; 
     let n2= number2;
     let total =0;
-    if(number1>number2){
+
+    if ( (typeof number1 != 'number') || (typeof number2 != 'number')){
+        return "ERROR";
+    }
+
+
+    else if(number1>number2){
         n2 = number1; // n1 will always be the smaller number
         n1=number2;
     }
+    
     if((n1 || n2 )<0){
         return "ERROR"
-    }else{
+    }
+    else{
         
         for(let i = n1; i<n2+1; i++){
             total = total + i;
