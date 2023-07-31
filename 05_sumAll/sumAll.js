8c819730d4b78db1c113cc4edf77473ac30aa4fa
plakas9 <plakas9@gmail.com>
2023-07-31 16:42:31 +0200

    passed first two test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(number1,number2) {
+    let n1 = number1; 
+    let n2= number2;
+    let total =0;
+    if(number1>number2){
+        n2 = number1; // n1 will always be the smaller number
+        n1=number2;
+    }
+
+    for(let i = number1; i<number2+1; i++){
+        total = total + i;
+    }
+    return total;
 
 };
 
