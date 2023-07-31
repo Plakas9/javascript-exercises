9c66a23420cd9b0afafd256ceefa2970de5c4c21
plakas9 <plakas9@gmail.com>
2023-07-31 14:47:38 +0200

    passed case for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,13 @@
 const repeatString = function(word,times) {
     const joinArray = [];
-    for(let i =  0; i< times; i++){
-        joinArray.push(word);
+    if(times < 0 ){
+        return 'ERROR'
+    }else{
+        for(let i =  0; i< times; i++){
+            joinArray.push(word);
+        }
+        return joinArray.join("");
     }
-    return joinArray.join("")   ;
 };
 
 // Do not edit below this line
